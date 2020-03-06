1e8c20a04e4458accdc261c98c05d9aea607695e
Altyn Berdiyeva <aberdiyeva@lisaacademy.org>
2020-03-05 18:07:26 -0600

    mvp code is here

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import "./App.css";
+import Image from "./Image";
+import axios from "axios";
+
+
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect ( () => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY').then(responce => {
+      setData(responce.data);
+      console.log(responce)
+    })
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <h1 className="title">{data.title}</h1>
+      <p className="parag">{data.explanation}</p>
+      <Image image={data.url}/>
     </div>
   );
 }
